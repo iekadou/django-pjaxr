8167b388a6f7d85e500033f2de815371429f7d93
Jonas <jbraun@uni-koblenz.de>
2014-03-22 10:35:03 +0100

    passing request to pjaxr-methods

--- django_pjaxr/mixins.py
+++ django_pjaxr/mixins.py
@@ -13,21 +13,21 @@ class PjaxrMixin(View):
 
     def dispatch(self, request, *args, **kwargs):
         super(PjaxrMixin, self).__init__()
-        matching_count = self.get_matching_count()
+        matching_count = self.get_matching_count(request)
         self.pjaxr_site = matching_count <= 0
         self.pjaxr_page = matching_count <= 1
         self.pjaxr_content = matching_count <= 2
         self.pjaxr_inner_content = matching_count <= 3
         return super(PjaxrMixin, self).dispatch(request, *args, **kwargs)
 
-    def get_matching_count(self):
+    def get_matching_count(self, request):
         """
         takes current_namespace to return the matching namespaces of the previous pjaxr-request and the current
         """
-        if not self.is_pjaxr_request():
+        if not self.is_pjaxr_request(request):
             return 0
         current_namespaces = self.namespace.split(".")
-        previous_namespaces = self.get_previous_namespace().split(".")
+        previous_namespaces = self.get_previous_namespace(request).split(".")
         level = 0
         matching_count = 0
         while level < len(previous_namespaces) and level < len(current_namespaces):
@@ -38,11 +38,10 @@ class PjaxrMixin(View):
                 break
         return matching_count
 
-    def get_previous_namespace(self, *args, **kwargs):
-        meta = self.request.META
-        if self.is_pjaxr_request() and meta.get('HTTP_X_PJAX_NAMESPACE', False):
-            return meta['HTTP_X_PJAX_NAMESPACE']
+    def get_previous_namespace(self, request):
+        if self.is_pjaxr_request(request) and request.META.get('HTTP_X_PJAX_NAMESPACE', False):
+            return request.META['HTTP_X_PJAX_NAMESPACE']
         return ""
 
-    def is_pjaxr_request(self):
-        return True if 'HTTP_X_PJAX' in self.request.META else False
+    def is_pjaxr_request(self, request):
+        return True if request.META.get('HTTP_X_PJAX_NAMESPACE', False) else False
